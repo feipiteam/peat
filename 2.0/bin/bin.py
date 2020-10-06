082377e6fed31129bed5ed24e8896e0e7bce723e
Feipi <3068315381@qq.com>
2020-10-06 15:37:39 +0800

    Feipi

---
+++ 2.0/bin/bin.py
@@ -0,0 +1,16 @@
+AI=input("欢迎来到闯关大世界！是否参加(Y/N)")
+if AI == "Y" :
+    print("第一关，CPU出事了")
+else :
+    print("再见")
+    exit()
+print("当前CPU码率：0000010000010000000000100000000000000001000000000000000000")
+AI=input("你不觉得有问题吗？(Y/N)")
+if AI == "Y" :
+    print("是的！他确实有点问题了。")
+else :
+    print("再见")
+    exit()
+print("让我们帮助CPU吧！")
+print("未完待续！")
+exit()
